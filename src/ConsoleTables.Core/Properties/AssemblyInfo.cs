4f3587fcb6826d0e3ca81a32e4065778fc65c6e9
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2016-06-23 18:15:45 -0400

    bumped down netstandard 1.4

--- src/ConsoleTables.Core/Properties/AssemblyInfo.cs
+++ src/ConsoleTables.Core/Properties/AssemblyInfo.cs
@@ -1,19 +1,35 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("ConsoleTables.Core")]
+[assembly: AssemblyDescription("Beautiful Console Tables")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConsoleTables.Core")]
+[assembly: AssemblyCopyright("Copyright © Khalid Abuhakmeh 2014")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db4b8e58-512e-406d-99bb-8a5e7690c9ec")]
+[assembly: Guid("061f2f6f-eeff-4f42-8df7-3dffef0165a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
